c5c7c8cff7c6fcada37c927323a72ce4ed104f1a
Sámuel Léránt <lerantsamuel@gmail.com>
2021-10-20 22:02:56 +0200

    Repository and Irepository skeletons added

---
+++ AKRYTN_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKRYTN_HFT_2021221.Repository
+{
+    // The skeleton for all descending repositories
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        
+        // The generic getall method
+        IQueryable<TEntity> GetAll();
+
+        
+        // The generic get one method
+        TEntity GetOneById(int id);
+
+        // The generic insert method
+        void Insert(TEntity entity);
+
+        // The generic remove method
+        void Remove(int id);
+    }
+    
+}
---
+++ AKRYTN_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using AKRYTN_HFT_2021221_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKRYTN_HFT_2021221.Repository
+{
+
+    public abstract class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+
+        protected BookStoreDbContext dbContext = new BookStoreDbContext();
+
+        public Repository(BookStoreDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return this.dbContext.Set<TEntity>();
+        }
+
+
+        public abstract TEntity GetOneById(int id);
+
+
+        public void Insert(TEntity entity)
+        {
+            this.dbContext.Set<TEntity>().Add(entity);
+            this.dbContext.SaveChanges();
+        }
+
+
+        public void Remove(int id)
+        {
+            this.dbContext.Set<TEntity>().Remove(this.GetOneById(id));
+            this.dbContext.SaveChanges();
+        }
+    }
+}
