e9746d23483fe7bb951a8c044a1fe81a4dd21f59
Sámuel Léránt <lerantsamuel@gmail.com>
2021-10-23 18:21:31 +0200

    OrderCart_Connector Repository Finished

---
+++ AKRYTN_HFT_2021221.Repository/IConnectorRepository.cs
@@ -0,0 +1,16 @@
+﻿using AKRYTN_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKRYTN_HFT_2021221.Repository
+{
+    public interface IConnectorRepository : IRepository<OrderCart_Connector>
+    {
+        void UpdateCartId(int id, int newCartId);
+
+        void UpdateItemId(int id, int newItemId);
+    }
+}
