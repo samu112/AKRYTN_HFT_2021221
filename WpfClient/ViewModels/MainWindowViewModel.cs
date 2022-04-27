07c40eda35197a5b198ba6dc76518a20bddc8ff9
Sámuel Léránt <lerantsamuel@gmail.com>
2022-04-27 13:02:39 +0200

    Main Windows GUI and it's view model's basics are set

---
+++ WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+
+    }
+}
